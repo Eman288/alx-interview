62bceca90df14d1804b7950c4e367fe412f1a7ea
Eman288 <emantamam107@gmail.com>
2024-11-11 20:31:22 +0000

    fix the code style

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,16 +2,17 @@
 """
     a module to get all the solutions for the n-queens problem
 """
-import sys;
+import sys
 
 
 def solveNQueens(n):
     """
     Solves the n-queens problem and returns all valid solutions.
     """
-    # This will store all the solutions in terms of column positions for each row
+    # This will store all the solutions in terms of column positions for each
+    # row
     solutions = []
-    
+
     # Column, Diagonal 1, Diagonal 2 trackers
     attacked_columns = [False] * n
     attacked_diag1 = [False] * (2 * n - 1)
@@ -23,28 +24,31 @@ def solveNQueens(n):
     def backtrack(row):
         # If we've placed queens in all rows, we found a solution
         if row == n:
-            solutions.append(current_solution[:])  # Store a copy of the solution
+            solutions.append(current_solution[:])  # Store the solution
             return
-        
+
         for col in range(n):
             # Check if the column or diagonals are attacked
-            if attacked_columns[col] or attacked_diag1[row - col + n - 1] or attacked_diag2[row + col]:
+            if attacked_columns[col] or attacked_diag1[row - col + n - 1]
+            or attacked_diag2[row + col]:
                 continue
-            
+
             # Place queen on the current row, column
             current_solution[row] = col
-            attacked_columns[col] = attacked_diag1[row - col + n - 1] = attacked_diag2[row + col] = True
-            
+            attacked_columns[col] = attacked_diag1[row - col + n - 1] = False
+            attacked_diag2[row + col] = True
+
             # Move to the next row
             backtrack(row + 1)
-            
+
             # Backtrack, remove queen and reset attacks
-            attacked_columns[col] = attacked_diag1[row - col + n - 1] = attacked_diag2[row + col] = False
+            attacked_columns[col] = attacked_diag1[row - col + n - 1] = False
+            attacked_diag2[row + col] = False
             current_solution[row] = -1
 
     # Start solving from the first row
     backtrack(0)
-    
+
     return solutions
 
 
@@ -52,10 +56,11 @@ def printSolution(sol):
     """
         a function to print the solution in a list like
     """
-    l = []
+    solList = []
     for i in range(len(sol)):
-        l.append([i, sol[i]])
-    print(l)
+        solList.append([i, sol[i]])
+    print(solList)
+
 
 def main():
     """
@@ -70,18 +75,15 @@ def main():
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    
+
     if num < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
-    solutions = solveNQueens(num)
-    
 
+    solutions = solveNQueens(num)
     for solution in solutions:
-        ##printBoard(num, solution)
+        # print each solution
         printSolution(solution)
 
 
 main()
-
