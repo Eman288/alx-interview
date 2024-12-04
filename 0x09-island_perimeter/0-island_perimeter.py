ef1f28b5a5727f7a46991f849e0be2f3da5aedf6
Eman288 <emantamam107@gmail.com>
2024-12-04 15:28:44 -0800

    fixing task -

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,15 +9,17 @@ def island_perimeter(grid):
     a function to get the island perimerter
     """
     n = len(grid)
+    m = len(grid[0])
     s = 0
-    for i in range(1, n - 1):
-        for j in range(1, len(grid[i]) - 1):
-            if grid[i][j] == 1 and grid[i - 1][j] == 0:
-                s += 1
-            if grid[i][j] == 1 and grid[i][j - 1] == 0:
-                s += 1
-            if grid[i][j] == 1 and grid[i + 1][j] == 0:
-                s += 1
-            if grid[i][j] == 1 and grid[i][j + 1] == 0:
-                s += 1
+    for i in range(0, n):
+        for j in range(0, m):
+            if grid[i][j] == 1:
+                if i != 0 and grid[i - 1][j] == 0:
+                    s += 1
+                if j != 0 and grid[i][j - 1] == 0:
+                    s += 1
+                if i != n - 1 and grid[i + 1][j] == 0:
+                    s += 1
+                if j != m - 1 and grid[i][j + 1] == 0:
+                    s += 1
     return s
